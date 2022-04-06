699a57a908a33a6165de73f0cde4be1aa54a7079
Colin McHale <93059063+colinmchale@users.noreply.github.com>
2022-04-06 12:43:29 -0500

    rearranged all to one single page with multiple sections, Navbar always at top

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React from "react";
+import NavTabs from './components/NavTabs';
 import PortfolioContainer from "./components/PortfolioContainer";
 
-const App = () => <PortfolioContainer />;
+const App = () => <div>
+    {/* We are passing the currentPage from state and the function to update it */}
+    <NavTabs/>
+    {/* currentPage={currentPage} handlePageChange={handlePageChange} */}
+    <PortfolioContainer/>
+    </div>;
 
 export default App;
